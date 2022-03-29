7bd1bf3c3e1dd211114c8b9b53a35423f76848e3
Sripaul <sripaul.ca@gmail.com>
2022-03-29 13:35:03 -0700

    adding logs to verify

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.send("<h1>Hello From MKT - 1</h1>")
+    console.log('Sripaul was here');
+    return res.send("<h1>Hello From MKT - 1</h1>")
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(

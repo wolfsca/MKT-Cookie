53f12f6ac58f40fecbf3a8d86cbb9023c1e247ab
Sripaul <sripaul.ca@gmail.com>
2022-03-29 13:32:20 -0700

    adding text changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.send("<h1>Hello From MKT</h1>")
+    res.send("<h1>Hello From MKT - 1</h1>")
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(

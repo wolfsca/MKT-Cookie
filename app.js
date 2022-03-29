da743381185c1bc838b7eaa02d29d08d6ce0652b
Sripaul <sripaul.ca@gmail.com>
2022-03-29 13:42:38 -0700

    adding new route

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 app.get("/", function(req, res) {
-    console.log('Sripaul was here', req.cookies);
+    console.log('Sripaul was here', JSON.stringify(req.cookies));
     var randomNumber=Math.random().toString();
     randomNumber=randomNumber.substring(2,randomNumber.length);
     res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
     return res.send("<h1>Hello From MKT - 1</h1>")
+});
+
+app.get("/getCookieInfo", function(req, res) {
+    console.log('Sripaul get cookie info here', JSON.stringify(req.cookies));
+    return res.send("success");
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(

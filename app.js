2cfc3e70f0b092518e45d0b2c6a3970851eea984
Sripaul <sripaul.ca@gmail.com>
2022-03-29 13:38:54 -0700

    adding cookies

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const cookieParser = require("cookie-parser");
 
 app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", function(req, res) {
-    console.log('Sripaul was here');
+    console.log('Sripaul was here', req.cookies);
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
     return res.send("<h1>Hello From MKT - 1</h1>")
 });
 

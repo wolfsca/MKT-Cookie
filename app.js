611371f82169526757d0ecf6a89c9588254b4728
Sripaul <sripaul.ca@gmail.com>
2022-03-29 13:14:15 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello From MKT</h1>")
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(
+    "Server is running..."
+));
